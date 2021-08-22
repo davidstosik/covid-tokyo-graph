db6a250f193bed372737d3b4513c28eee4c04c6a
David Stosik <david.stosik+git-noreply@gmail.com>
2021-08-22 19:44:37 +0900

    Extract constants

--- check_opendata_updates.py
+++ check_opendata_updates.py
@@ -1,15 +1,11 @@
 import requests
 import hashlib
-
-URIS = {
-  'details': 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_details_testing_positive_cases.csv',
-  'patients': 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv'
-}
+import constants
 
 with open('./data_hashes.csv', 'w', newline='') as output:
     print('file,bytes,md5', file=output)
 
-    for key, uri in URIS.items():
+    for key, uri in constants.URIS.items():
         response = requests.get(uri)
 
         row = [
---
+++ constants.py
@@ -0,0 +1,4 @@
+URIS = {
+  'details': 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_details_testing_positive_cases.csv',
+  'patients': 'https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv'
+}
